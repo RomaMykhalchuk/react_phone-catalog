09bb36da606ac60e5f3a3ee29afa8f5c4589d919
Roma <roman.mykhalchuk@yahoo.com>
2020-05-27 00:26:18 +0300

    Updates

---
+++ precache-manifest.4b571389372bb143fd7ac3a9a6039b39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26668350f1005351ee5d96c60586f9b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d5ee4386ff26720d153",
+    "url": "./static/css/main.69130e47.chunk.css"
+  },
+  {
+    "revision": "a0b85091d9bdddc6d9e7",
+    "url": "./static/js/2.445086b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.445086b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5ee4386ff26720d153",
+    "url": "./static/js/main.05038154.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.4b571389372bb143fd7ac3a9a6039b39.js"
 );
 
 self.addEventListener('message', (event) => {
