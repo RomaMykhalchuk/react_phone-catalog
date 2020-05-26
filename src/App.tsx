67de6892672ea3b9f488a8f1c7ed5ca964f0f2f0
Roma <roman.mykhalchuk@yahoo.com>
2020-05-27 00:25:12 +0300

    header done, created variables and mixins storage, changed app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { Route, HashRouter } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 import './App.scss';
 
 const App = () => (
   <div className="App">
-    <h1>React Phone Catalog</h1>
+    <HashRouter>
+      <Header />
+      <Route path="/" exact />
+      <Route path="/phones" />
+      <Route path="/accesories" />
+      <Route path="/tablets" />
+      <Route path="/accesories" />
+      <Footer />
+    </HashRouter>
   </div>
 );
 
